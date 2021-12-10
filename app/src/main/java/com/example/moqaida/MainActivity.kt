3963fa09bb53df7af0e3ed8d462d2391ab113273
zainab ALjutayl <zainab.Aljutayl.dmm.k1@tuwaiq.edu.sa>
2021-12-11 02:40:01 +0300

    "making some of xml files & all icons"

--- app/src/main/java/com/example/moqaida/MainActivity.kt
+++ app/src/main/java/com/example/moqaida/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.moqaida
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moqaida.databinding.ActivityMainBinding
+import com.example.moqaida.views.MainActivity2
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+//        binding.signUpButton.setOnClickListener {
+//
+//            val intent = Intent(this,MainActivity2::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
     }
 }
