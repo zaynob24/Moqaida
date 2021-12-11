99dcc363c44963f840f930bba84eb355a13fb64c
zainab ALjutayl <zainab.Aljutayl.dmm.k1@tuwaiq.edu.sa>
2021-12-11 21:42:28 +0300

    "continues with xml files"

--- app/src/main/java/com/example/moqaida/MainActivity.kt
+++ app/src/main/java/com/example/moqaida/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.moqaida
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.moqaida.databinding.ActivityMainBinding
 import com.example.moqaida.views.MainActivity2
 
@@ -21,5 +24,14 @@ class MainActivity : AppCompatActivity() {
 //            startActivity(intent)
 //            finish()
 //        }
+
+        //requireContext()
+        val items = listOf("Option 1", "Option 2", "Option 3", "Option 4")
+        val adapter = ArrayAdapter(this, R.layout.list_item_years, items)
+        (binding.usedForMenu.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+        (binding.locationMenu.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+
+
+
     }
 }
