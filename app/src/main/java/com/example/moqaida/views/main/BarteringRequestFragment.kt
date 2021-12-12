d93fed7d254f74b482dcdd4ef690529a2db3c4ed
zaynob24 <Zainab.Aljutayl.dmm.k1@tuwaiq.edu.sa>
2021-12-12 16:51:37 +0300

    "making xml files ..."

--- app/src/main/java/com/example/moqaida/views/main/BarteringRequestFragment.kt
+++ app/src/main/java/com/example/moqaida/views/main/BarteringRequestFragment.kt
@@ -5,21 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import com.example.moqaida.R
 
 
-class AddItemFragment : Fragment() {
-
+class BarteringRequestFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_item, container, false)
+        return inflater.inflate(R.layout.fragment_bartering_request, container, false)
     }
-
-
 
 }
