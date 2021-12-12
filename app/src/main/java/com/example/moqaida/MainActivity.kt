0ec5fe0a74f1ea3528f24d6a5c259ef31f197a31
zaynob24 <Zainab.Aljutayl.dmm.k1@tuwaiq.edu.sa>
2021-12-13 00:59:48 +0300

    "Navigation Drawer"

--- app/src/main/java/com/example/moqaida/MainActivity.kt
+++ app/src/main/java/com/example/moqaida/MainActivity.kt
@@ -3,19 +3,45 @@ package com.example.moqaida
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.moqaida.databinding.ActivityMainBinding
 import com.example.moqaida.views.MainActivity2
+import com.example.moqaida.views.idintity.LoginFragment
+import com.example.moqaida.views.main.AddItemFragment
+import com.example.moqaida.views.main.HomeFragment
+import com.example.moqaida.views.main.MyItemFragment
+
+//Navigation Drawer with Fragments - Navigation Component
+//https://www.youtube.com/watch?v=PvuaPL4D-N8&ab_channel=CodeWithMazn
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        navController = findNavController(R.id.fragmentContainerView)
+        binding.navigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph,binding.drawerLayout)
+
+        setupActionBarWithNavController(navController,appBarConfiguration)
 
 
 //        binding.signUpButton.setOnClickListener {
@@ -32,6 +58,10 @@ class MainActivity : AppCompatActivity() {
 //        (binding.locationMenu.editText as? AutoCompleteTextView)?.setAdapter(adapter)
 
 
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val  navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
